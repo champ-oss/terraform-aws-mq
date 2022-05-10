0ec460a7fee62f871b9bcbdeb154e75a97eda2d9
Phillip Langley <philliplangley@champmbplangley.lan>
2022-05-10 09:35:50 -0500

    add prevent destroy on mq broker

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -15,12 +15,12 @@ func TestExamplesCluster(t *testing.T) {
 	terraformOptions := &terraform.Options{TerraformDir: "../../examples/complete"}
 	defer terraform.Destroy(t, terraformOptions)
 
-    terraform.Init(t, terraformOptions)
-    // recursively set prevent destroy to false
-    cmd := exec.Command("bash", "-c", "find . -type f -name '*.tf' -exec sed -i'' -e 's/prevent_destroy = true/prevent_destroy = false/g' {} +")
-    cmd.Dir = "../../"
-    cmd.Run()
-    terraform.ApplyAndIdempotent(t, terraformOptions)
+	terraform.Init(t, terraformOptions)
+	// recursively set prevent destroy to false
+	cmd := exec.Command("bash", "-c", "find . -type f -name '*.tf' -exec sed -i'' -e 's/prevent_destroy = true/prevent_destroy = false/g' {} +")
+	cmd.Dir = "../../"
+	cmd.Run()
+	terraform.ApplyAndIdempotent(t, terraformOptions)
 
 	logger.Log(t, "validating console_url exist and not empty")
 	// Run `terraform output` to get the value of an output variable
