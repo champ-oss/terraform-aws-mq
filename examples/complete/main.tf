c23258df7f6836fb71d283015456b816aa26b58d
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-10-02 13:20:29 -0500

    update output vars

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -51,3 +51,34 @@ module "single_instance" {
   use_aws_owned_key          = true
   auto_minor_version_upgrade = true
 }
+
+output "console_url" {
+  value       = module.cluster.console_url
+  description = "Rabbit console URL"
+}
+
+output "primary_amqp_ssl_endpoint" {
+  value       = module.cluster.primary_amqp_ssl_endpoint
+  description = "AmazonMQ primary AMQP+SSL endpoint"
+}
+
+output "broker_host" {
+  value       = trim(module.cluster.console_url, "https://")
+  description = "console host plus protocol"
+}
+
+output "broker_pw" {
+  value       = module.cluster.password
+  sensitive   = true
+  description = "AmazonMQ primary AMQP+SSL endpoint"
+}
+
+output "broker_id" {
+  value       = module.cluster.id
+  description = "rabbit broker id"
+}
+
+output "broker_arn" {
+  value       = module.cluster.arn
+  description = "rabbit broker arn"
+}
