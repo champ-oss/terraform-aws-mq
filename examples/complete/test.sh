db2e86f12508c593e65dc1c02788518483f5c89d
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2026-01-29 08:40:48 -0600

    Update test.sh

--- examples/complete/test.sh
+++ examples/complete/test.sh
@@ -1,7 +1,7 @@
 set -e
 
 if [[ ! "$BROKER_HOST" =~ \.on\.aws$ ]]; then
-  echo "❌ broker_host does not end with .on.aws"
+  echo "broker_host does not end with .on.aws"
   echo "   Value: $BROKER_HOST"
   exit 1
 fi
